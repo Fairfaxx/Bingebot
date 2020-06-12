fc0746203a787c3dbab61acb4b4b2dd3e1b24e1c
Fefe Lemaire <fede.lemaire@gmail.com>
2020-06-12 11:57:27 -0300

    adding iboutlets and ibactions

--- BingeBot/ViewController.swift
+++ BingeBot/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showLbl: UILabel!
+    @IBOutlet weak var randomShowLbl: UILabel!
+    @IBOutlet weak var bingebotSpokenLbl: UILabel!
+    @IBOutlet weak var addShowTextField: UITextField!
+    @IBOutlet weak var addShowBtn: UIButton!
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    @IBOutlet weak var addShowStackView: UIStackView!
+    @IBOutlet weak var showsStackView: UIStackView!
+    
+    var shows: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    func updateShowsLabel() {
+        showLbl.text = shows.joined(separator: ", ")
+    }
+    
+    @IBAction func addShowBtnPressed(_ sender: Any) {
+        guard let showName = addShowTextField.text else {return}
+        shows.append(showName)
+        updateShowsLabel()
+        addShowTextField.text = ""
+        
+    }
+    
+    
+    
+    
 
 }
 
